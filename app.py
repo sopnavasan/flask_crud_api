02ed010c245a6bb79c0b6adb72e846dad740720f
Sopna <sopnavasan@gmail.com>
2026-05-24 23:17:59 +0530

    error fixed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import OperationalError, IntegrityError
 from datetime import datetime
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root123@localhost/flask_crud_db2"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
