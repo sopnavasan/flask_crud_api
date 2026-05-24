58d7e40747bf03a788de50128720c26cad582a78
Sopna <sopnavasan@gmail.com>
2026-05-24 22:49:55 +0530

    MySQL connection added

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import OperationalError, IntegrityError
 from datetime import datetime
 
+app = Flask(_name_)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root123@localhost/flask_crud_db2"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
